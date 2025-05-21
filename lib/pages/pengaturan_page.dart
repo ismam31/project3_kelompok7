aca583abd41188039fb38d96a0a71f7d609e0fb3
ilhamcomrade <ilhamwiguna2005@gmail.com>
2025-05-21 08:24:49 +0700

    menambahkan isi pada halaman pengaturan

--- lib/pages/pengaturan_page.dart
+++ lib/pages/pengaturan_page.dart
@@ -15,7 +15,34 @@ class PengaturanPage extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: [
-          
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('Profil Akun'),
+            onTap: () {
+              // Navigasi atau aksi ke halaman profil
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Pengaturan'),
+            onTap: () {
+              // Sudah di halaman ini, mungkin bisa disable atau beri indikasi
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.info),
+            title: const Text('Tentang Aplikasi'),
+            onTap: () {
+              // Navigasi ke halaman tentang aplikasi
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout, color: Colors.red),
+            title: const Text('Logout'),
+            onTap: () {
+            Navigator.pushNamed(context, '/dashboard');
+            },
+          ),
         ],
       ),
     );
